727bc8cc98dddb768071038024e5c220502d4668
jorgeflagel <jorgeflagel@gmail.com>
2021-05-16 20:22:09 -0500

    Conectando componente Home con la API para acceder a la informacion del equipo, a partir de la lista de IDs. Mostrando una lista de que incluye nombre, imagen y powerstats de los heroes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
 export default function Home({ equipo }) {
+
+    const [equipoInfo, setEquipoInfo] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        equipo.forEach((id) => {     
+            axios.get(`https://www.superheroapi.com/api.php/${process.env.REACT_APP_API_TOKEN}/${id}`)
+            .then((res) => {
+                if (res.data.response === "success") {
+                    setError(null);
+                    setEquipoInfo((equipoPrevio) => [...equipoPrevio, res.data]);
+                }
+                else {
+                    setError(res.data.error);
+                }})
+            .catch((error) => setError(error))
+        })}, [equipo]
+    )
+
     return (
         <div>
             <h1>Página Home</h1>
-            <ol>
-                {equipo.map((id) => <li>{id}</li>)}
-            </ol>
+            <ul>
+                {equipoInfo.map((heroe) => {
+                    return(
+                        <li key={heroe.id}>
+                            <h2>{heroe.name}</h2>
+                            <img src={heroe.image.url}/>
+                            <p>{JSON.stringify(heroe.powerstats)}</p>
+                            
+                        </li>
+                        )
+                    })
+                }
+            </ul>
         </div>
     )
 }
