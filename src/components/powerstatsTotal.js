27cf9a3f315b8ccf2408fd52fd219dfc2588995f
jorgeflagel <jorgeflagel@gmail.com>
2021-07-13 19:46:25 -0500

    deleting warning

--- src/components/powerstatsTotal.js
+++ src/components/powerstatsTotal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import sortObject from '../utils/sortObject';
 
 export default function PowerstatsTotal( { powerstatsTotal, numHeroes } ) {
